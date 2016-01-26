d90d1bb3b9d1a2d9284e14eaf9967a5bca1e23d3
Andy <aschlali@uci.edu>
2016-01-25 20:17:40 -0800

    Improved count down timer interval destruction and count down page

--- www/scripts/CountdownCtrl.js
+++ www/scripts/CountdownCtrl.js
@@ -4,14 +4,32 @@ app.controller('CountdownCtrl',function($scope, $interval, $timeout){
 //Ingame Events. Times in UTC
 $scope.igevents = [{Name:"Noon Flag Race",Hour:"20",Minute:"0"},{Name:"7pm Flag Race",Hour:"3",Minute:"0"},{Name:"9pm Flag Race",Hour:"5",Minute:"0"},{Name:"10pm Flag Race",Hour:"6",Minute:"0"},{Name:"11pm Flag Race",Hour:"7",Minute:"0"},{Name:"Daily Server Reset",Hour:"8",Minute:"0"}];
 
+var cdinter;
 $scope.countdowns = function(){
-	$interval(function(){
+	//checks if interval has started
+	if (angular.isDefined(cdinter))
+		return;
+	
+	cdinter = $interval(function(){
 		for (var i=0;i<$scope.igevents.length;i++){
 			$scope.igevents[i].countdown = CDtimers.remainingTime($scope.igevents[i].Hour,$scope.igevents[i].Minute);
 		}
 	}
 	, 1000);
 };
+
+//stops interval
+$scope.stopinter = function(){
+	if(angular.isDefined(cdinter)){
+		$interval.cancel(cdinter);
+		cdinter = undefined;
+	}
+};
+
+//When switching pages, stops the interval.
+$scope.$on("$destroy", function(){
+	$scope.stopinter();
+});
 
 $timeout($scope.countdowns);
 
