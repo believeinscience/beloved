34db93e1e1bb59fb962a570d9f87c0f7fb543790
Andy <aschlali@uci.edu>
2016-01-29 20:17:02 -0800

    Improved grunt build to include other page's html into main index html

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,18 +18,31 @@ concat: {
 	files:[
 		{src: ['www/library/underscore.deferred.js','www/library/underscore.math.js','www/library/miso.events.js','www/library/miso.ds.0.4.1.js','www/scripts/spriteAnimation.js','www/scripts/countdowns.js','www/scripts/pagemanager.js','www/scripts/CountdownCtrl.js','www/scripts/scammerController.js'], dest: 'temp/scripts/scripts.js'}
 	]
+  },
+  test:{
+	options:{
+		process: function(src, filepath){
+			return '<script type="text/ng-template" id="'+filepath.substring(5,filepath.length)+'">'+src+"</script>";
+		}
+	},
+	files:[
+		{src:['temp/pages/about.html','temp/pages/Countdown.html','temp/pages/home.html','temp/pages/ScammerReg.html'],dest:"temp/pages/concatted.html"}
+	]
   }
 },
 minifyHtml:{
 	options:{
 		cdata: true
 	},
-	release:{
+	mainindex:{
 		files:[
-			{src:"temp/index.html",dest:"build/index.html"},
-			{expand: true,cwd: 'www/pages',src:['*'],dest:'build/pages/'}
+			{src:"temp/index.html",dest:"build/index.html"}
 		]
-
+	},
+	pageminify:{
+			files:[
+			{expand: true,cwd: 'www/pages',src:['*'],dest:'temp/pages/'}
+		]
 	}
 },
 cssmin:{
@@ -47,7 +60,9 @@ includereplace: {
       globals: {
         mainscripts: "<script src='scripts/scripts.js'></script>",
 		commentstart: "<!--",
-		commentend: "-->"
+		commentend: "-->",
+		beginstyletag: "<style>",
+		endstyletag: "</style>"
       },
       prefix: '<!-- @@',
       suffix: ' -->'
@@ -76,6 +91,5 @@ grunt.loadNpmTasks('grunt-include-replace');
 grunt.loadNpmTasks('grunt-contrib-copy');
 grunt.loadNpmTasks('grunt-contrib-concat');
 // Default task(s).
-grunt.registerTask('release', ['includereplace','concat','uglify','minifyHtml','cssmin','copy']);
-
+grunt.registerTask('release', ['minifyHtml:pageminify','concat','includereplace','uglify','minifyHtml:mainindex','cssmin','copy']);
 };
