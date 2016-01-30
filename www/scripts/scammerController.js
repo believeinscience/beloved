addbca3dac124ddb1f40515d90167d5415513261
Andy <aschlali@uci.edu>
2016-01-29 21:30:03 -0800

    scammer registry now lists all entries upon retrieving them.

--- www/scripts/scammerController.js
+++ www/scripts/scammerController.js
@@ -23,9 +23,10 @@ $scope.getdata = function() {$scope.ds.fetch({
   success : function() {
   
 	//put data into easy to read format from miso
+	var data = []
 	for (var i=0;i<this.length; i++)
-		$scope.retrieveddata.push(this.rowByPosition(i)); 
-
+		data.push(this.rowByPosition(i)); 
+	$scope.$apply(function(){$scope.retrieveddata = data;});
 	//add data to drop down
 	//$scope.$apply(function(){  $scope.searchcriteria.values = $scope.ds.columnNames();});
 	
