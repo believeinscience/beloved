bc63d24c7686bb47135b0cd3b678a701865a9fbf
Andy <aschlali@uci.edu>
2016-02-01 20:46:30 -0800

    Rearranged entries in scammer list to be newest first by default

--- www/scripts/scammerController.js
+++ www/scripts/scammerController.js
@@ -26,6 +26,13 @@ $scope.getdata = function() {$scope.ds.fetch({
 	var data = [];
 	for (var i=0;i<this.length; i++)
 		data.push(this.rowByPosition(i)); 
+	//change dates to date objects (moment)
+	for (i=0;i<data.length; i++){
+		//a bit of a hack at getting it to order by correctly since null values order themselves towards the top before most recent
+		if(data[i].Date===null)
+			data[i].Date = new moment("1/1/1000");
+		data[i].Date = new moment(data[i].Date);
+	}
 	$scope.$apply(function(){$scope.retrieveddata = data;});
 	//add data to drop down
 	//$scope.$apply(function(){  $scope.searchcriteria.values = $scope.ds.columnNames();});
