8e83342e5e034f6ece8a4d081603e5b14e695131
Andy <aschlali@uci.edu>
2016-02-07 13:28:05 -0800

    Added Archer, compressed pics and added bamboo label

--- www/scripts/memberpageCtrl.js
+++ www/scripts/memberpageCtrl.js
@@ -12,7 +12,12 @@ namelabel:{type:"59",text:"whanderlust"},
 coord:{x:450,y:425},
 animationinfo:{frames:16,fps:8,states:5,width:61,height:84}
 },
-
+{ign:"archer",
+medal:{type:"0",text:""},
+namelabel:{type:"58",text:"Archer"},
+coord:{x:500,y:425},
+animationinfo:{frames:16,fps:8,states:6,width:57,height:70}
+},
 //dirty hack since the $last item never seems to have its animation work.
 //always last
 {ign:"null",
@@ -60,12 +65,17 @@ $scope.$on("$destroy", function(){
 					$scope.players[0].animation = new spriteAnima('#avatar_'+$scope.players[0].ign,$scope.players[0].animationinfo.fps,$scope.players[0].animationinfo.frames,$scope.players[0].animationinfo.states);
 					$scope.players[0].animation.setrandexpr([{state:2,weight:1,length:1000},{state:3,weight:1,length:1000}]);
 					$scope.players[0].animation.startinter();
-					$scope.players[0].onclick = function(){$scope.players[0].animation.doRandomExpression([{state:5,weight:1,length:3000},{state:6,weight:1,length:3000}],true);};
+					$scope.players[0].onclick = function(){$scope.players[0].animation.doRandomExpression([{state:5,weight:1,length:3000},{state:6,weight:1,length:3000},{state:7,weight:1,length:3000}],true);};
 
 					$scope.players[1].animation = new spriteAnima('#avatar_'+$scope.players[1].ign,$scope.players[1].animationinfo.fps,$scope.players[1].animationinfo.frames,$scope.players[1].animationinfo.states);
 					$scope.players[1].animation.setrandexpr([{state:2,weight:1,length:2000},{state:3,weight:1,length:2000}]);
 					$scope.players[1].animation.startinter();
 					$scope.players[1].onclick = function(){$scope.players[1].animation.doRandomExpression([{state:4,weight:1,length:3000},{state:5,weight:1,length:3000}],true);};
+					
+					$scope.players[2].animation = new spriteAnima('#avatar_'+$scope.players[2].ign,$scope.players[2].animationinfo.fps,$scope.players[2].animationinfo.frames,$scope.players[1].animationinfo.states);
+					$scope.players[2].animation.setrandexpr([{state:2,weight:1,length:2000},{state:3,weight:1,length:2000}]);
+					$scope.players[2].animation.startinter();
+					$scope.players[2].onclick = function(){$scope.players[2].animation.doRandomExpression([{state:4,weight:1,length:3000},{state:5,weight:1,length:3000},{state:6,weight:1,length:3000}],true);};
 					
 					for(var i=0;i<$scope.players.length-1;i++)
 						$('#player_'+$scope.players[i].ign).draggable();
